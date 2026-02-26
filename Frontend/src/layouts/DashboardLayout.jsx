31ed29c173d6430c16d9e5410f3ded07cad868a3
SubhasinghaD <dilankasuresh06@gmail.com>
2026-02-26 12:27:43 +0530

    Fix sidebar position - make it stay fixed when scrolling page

--- Frontend/src/layouts/DashboardLayout.jsx
+++ Frontend/src/layouts/DashboardLayout.jsx
@@ -4,9 +4,9 @@ import Sidebar from '../components/Sidebar/Sidebar';
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className='flex h-screen'>
+    <div className='flex h-screen overflow-hidden'>
       <Sidebar />
-      <div className='flex-1 flex flex-col'>
+      <div className='flex-1 flex flex-col overflow-auto'>
         <Navbar />
         <div className='p-6'>{children}</div>
       </div>
